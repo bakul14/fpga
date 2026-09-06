5d83747d57efed738d08d33ae3c3068e90b5e4bf
Mikhail Bakulevskiy <bakulevskiy140503@mail.ru>
2026-09-06 23:06:17 +0300

    ADD: ack flag before shading

--- semester_1/01_09_2026/task_1/shader.v
+++ semester_1/01_09_2026/task_1/shader.v
@@ -1,6 +1,7 @@
 module shader (
     input       clk,
     input       turn_request,
+    output wire ready_to_be_turned,
     output wire out0,
     output wire out1,
     output wire out2
@@ -8,9 +9,11 @@ module shader (
 
   reg [1:0] state;
 
+  assign ready_to_be_turned = 1'b1;  // stub, always ready
+
   always @(posedge clk) begin
 
-    if (turn_request) begin
+    if (turn_request && ready_to_be_turned) begin
       state <= state + 1;
       if (state >= 3) state <= 1;
     end
--- semester_1/01_09_2026/task_1/test.cpp
+++ semester_1/01_09_2026/task_1/test.cpp
@@ -11,10 +11,8 @@ public:
   {
     dut_->clk          = 0;
     dut_->turn_request = 0;
-    dut_->out0         = 0;
-    dut_->out1         = 0;
-    dut_->out2         = 0;
     dut_->eval();
+    EXPECT_EQ(dut_->ready_to_be_turned, 1);
   }
 };
 
@@ -22,9 +20,44 @@ TEST_F(ShaderTest, check_silence_when_no_request)
 {
   for (int i = 0; i < 1000; i++) {
     tick_();
+    while (!dut_->ready_to_be_turned) {
+      tick_();
+      EXPECT_EQ(dut_->out0, 0);
+      EXPECT_EQ(dut_->out1, 0);
+      EXPECT_EQ(dut_->out2, 0);
+    }
     EXPECT_EQ(dut_->out0, 0);
     EXPECT_EQ(dut_->out1, 0);
     EXPECT_EQ(dut_->out2, 0);
+  }
+}
+
+TEST_F(ShaderTest, check_silence_when_not_ready)
+{
+  dut_->turn_request = 1;
+
+  tick_();
+  while (!dut_->ready_to_be_turned) {
+    EXPECT_EQ(dut_->out0, 1);
+    EXPECT_EQ(dut_->out1, 0);
+    EXPECT_EQ(dut_->out2, 0);
+    tick_();
+  }
+
+  tick_();
+  while (!dut_->ready_to_be_turned) {
+    EXPECT_EQ(dut_->out0, 0);
+    EXPECT_EQ(dut_->out1, 1);
+    EXPECT_EQ(dut_->out2, 0);
+    tick_();
+  }
+
+  tick_();
+  while (!dut_->ready_to_be_turned) {
+    EXPECT_EQ(dut_->out0, 0);
+    EXPECT_EQ(dut_->out1, 0);
+    EXPECT_EQ(dut_->out2, 1);
+    tick_();
   }
 }
 
@@ -34,16 +67,19 @@ TEST_F(ShaderTest, check_shading_when_requested)
 
   for (int i = 0; i < 3; i++) {
     tick_();
+    while (!dut_->ready_to_be_turned);
     EXPECT_EQ(dut_->out0, 1);
     EXPECT_EQ(dut_->out1, 0);
     EXPECT_EQ(dut_->out2, 0);
 
     tick_();
+    while (!dut_->ready_to_be_turned);
     EXPECT_EQ(dut_->out0, 0);
     EXPECT_EQ(dut_->out1, 1);
     EXPECT_EQ(dut_->out2, 0);
 
     tick_();
+    while (!dut_->ready_to_be_turned);
     EXPECT_EQ(dut_->out0, 0);
     EXPECT_EQ(dut_->out1, 0);
     EXPECT_EQ(dut_->out2, 1);
