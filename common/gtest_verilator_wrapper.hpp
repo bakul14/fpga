2139f89efdca08326ac59f6a893a43c8f00d832e
Mikhail Bakulevskiy <mikhail.bakulevskiy@evocargo.com>
2026-09-07 07:55:15 +0300

    final instead of override

--- common/gtest_verilator_wrapper.hpp
+++ common/gtest_verilator_wrapper.hpp
@@ -15,7 +15,7 @@ template <typename Model>
 class VerilatorWrapperTestBase : public ::testing::Test
 {
 protected:
-  void SetUp() override
+  void SetUp() final
   {
     const std::string test_name = ::testing::UnitTest::GetInstance()->current_test_info()->name();
 
@@ -25,7 +25,7 @@ protected:
     reset_();
   }
 
-  void TearDown() override
+  void TearDown() final
   {
     dut_->final();
     trace_->close();
