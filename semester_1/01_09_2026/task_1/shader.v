b266d2c5d2568ed7154484c57924eb91a328b0c2
Mikhail Bakulevskiy <bakulevskiy140503@mail.ru>
2026-09-08 16:07:36 +0300

    ADD: timing support in simulation (#4)
    * ENH: real timing support
    
    * Update shader.v
    
    * ENH: external timescale config and interlan for shader as default
    
    * ENH: improved tests
    
    * ENH: update namestyle
    
    * ENH: upd cmake line length

--- semester_1/01_09_2026/task_1/shader.v
+++ semester_1/01_09_2026/task_1/shader.v
@@ -1,7 +1,9 @@
+`timescale 1ps / 1ps
+
 module shader (
     input       clk,
     input       turn_request,
-    output wire ready_to_be_turned,
+    output reg  ready_to_be_turned,
     output wire out0,
     output wire out1,
     output wire out2
@@ -9,19 +11,28 @@ module shader (
 
   reg [1:0] state;
 
-  assign ready_to_be_turned = 1'b1;  // stub, always ready
+  initial ready_to_be_turned = 1'b1;
+  initial state = 2'd0;
 
   always @(posedge clk) begin
-
     if (turn_request && ready_to_be_turned) begin
-      state <= state + 1;
-      if (state >= 3) state <= 1;
+      state              <= (state >= 3) ? 2'd1 : state + 2'd1;
+      ready_to_be_turned <= 1'b0;
+      #30 ready_to_be_turned <= 1'b1;
     end
-
   end
 
   assign out0 = (state == 1);
   assign out1 = (state == 2);
   assign out2 = (state == 3);
+
+  initial begin
+    #10
+    $display(
+        "Тестовая задержка инициализации, t = %0t", $realtime
+    );
+    wait (turn_request);
+    $display("Получен turn_request, t = %0t", $realtime);
+  end
 
 endmodule
